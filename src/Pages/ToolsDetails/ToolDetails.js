712c9d3074c88be0b1baf6ca42a7d8c60350f06d
pritom48 <pritamdas41148@gmail.com>
2022-05-24 11:29:00 +0600

    plase order added

--- src/Pages/ToolsDetails/ToolDetails.js
+++ src/Pages/ToolsDetails/ToolDetails.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { useParams } from 'react-router-dom';
+import auth from '../../firebase.init';
+
 
 const ToolDetails = () => {
     const { toolId } = useParams()
     const [tool, settool] = useState({})
+    const [user, loading, error] = useAuthState(auth);
 
     useEffect(() => {
         const url = `http://localhost:5000/tool/${toolId}`
@@ -14,8 +18,30 @@ const ToolDetails = () => {
 
     return (
         <div>
-            <h2>{toolId}</h2>
-            <h2>{tool.price}</h2>
+            <div class="card lg:card-side bg-base-300 shadow-xl my-20">
+                <figure><img className='w-96' src={tool.img} alt="Album" /></figure>
+                <div class="card-body">
+                    <h2 class="card-title text-3xl font-bold">{tool.name}</h2>
+                    <p>{tool.description}</p>
+                    <h2 className='text-xl '>Available Quantity- {tool.availableQuantity}</h2>
+                    <h2 className='text-xl '>Minimum Order Quantity- {tool.minOrderQuantity}</h2>
+                    <h1 className='text-4xl font-bold'>Price- ${tool.price}</h1>
+                </div>
+            </div>
+            <div className='flex justify-center'>
+                <div class="card w-96 bg-base-300 shadow-xl mb-20">
+                    <div class="card-body items-center text-center">
+                        <h2 class="card-title py-3">{tool.name}</h2>
+                        <input type="text" disabled value={user?.displayName || ''} placeholder="Type here" class="input w-full input-bordered max-w-xs" />
+                        <input type="text" disabled value={user?.email || ''} placeholder="Type here" class="input w-full input-bordered max-w-xs" />
+                        <input type="number" placeholder="Quantity" class="input w-full input-bordered max-w-xs" />
+                        <input type="text" placeholder="Address" class="input w-full input-bordered max-w-xs" />
+                        <input type="number" placeholder="Phone Number" class="input w-full input-bordered max-w-xs" />
+                        <input type="submit" value='Place order' class="btn btn-info w-full  max-w-xs" />
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
